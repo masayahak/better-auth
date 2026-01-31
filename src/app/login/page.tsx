6499334b6feffd0aaa6854b2265f197cd782d986
masayahak <masayahak@gmail.com>
2026-01-31 16:10:37 +0900

    ログインページのコード整形とテストアカウント作成の説明を追加

--- src/app/login/page.tsx
+++ src/app/login/page.tsx
@@ -1,5 +1,5 @@
 import Image from "next/image";
-import { LoginForm } from "@/components/login-form"
+import { LoginForm } from "@/components/login-form";
 
 export default function LoginPage() {
   return (
@@ -8,19 +8,17 @@ export default function LoginPage() {
         <div className="flex items-center gap-2 self-center font-medium">
           <div className="bg-primary text-primary-foreground flex size-6 items-center justify-center rounded-md">
             <div className="relative h-6 w-6">
-              <Image
-                src="/logo.png"
-                alt="ロゴ"
-                fill
-                className="object-contain"
-                sizes="24px"
-              />
+              <Image src="/logo.png" alt="ロゴ" fill className="object-contain" sizes="24px" />
             </div>
           </div>
           Hakamata soft
         </div>
         <LoginForm />
+        <div className="mt-4 text-center text-sm text-muted-foreground">
+          <p>テストアカウントも用意しています。</p>
+          <p className="font-mono mt-1">admin@test.com / kyouhayuki</p>
+        </div>{" "}
       </div>
     </div>
-  )
+  );
 }
